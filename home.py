dd1b657a6c34864c143e88ef0e2ef0e0e956f5d0
Zoha-Arif <Zoha-Arif@users.noreply.github.com>
2019-07-26 22:49:51 -0400

    finished user interfaceof homepage

--- home.py
+++ home.py
@@ -7,11 +7,18 @@ root = tk.Tk()
 HEIGHT = 700 
 WIDTH = 800 
 
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="#b5d6b2")
-canvas.pack()
+#canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="#FFFFFF")
+#canvas.pack()
+#widthxheight
+root.geometry("750x490")
+frame = tk.Frame(root, bg="#FFFFFF")
+#frame.place(relwidth=1, relheight=1)
+frame.pack()
 
-frame = tk.Frame(root, bg="#b5d6b2")
-frame.place(relwidth=1, relheight=1)
+home = tk.PhotoImage(file="home.png", height=500)
+home1 = tk.Label(frame, image=home,bg="#FFFFFF")
+home1.pack()
+#home1.place(relx=0.2,rely=0, relwidth=0.6, relheight=0.6)
 
 
 

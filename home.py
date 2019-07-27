1c8840e84ad72f168871ab08bb9c6827b9bcfee6
Zoha-Arif <Zoha-Arif@users.noreply.github.com>
2019-07-26 23:29:26 -0400

    worked on getting button working

--- home.py
+++ home.py
@@ -3,23 +3,20 @@ import requests
 import random 
 
 root = tk.Tk()
-
-HEIGHT = 700 
-WIDTH = 800 
-
-#canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="#FFFFFF")
-#canvas.pack()
 #widthxheight
 root.geometry("750x490")
 frame = tk.Frame(root, bg="#FFFFFF")
-#frame.place(relwidth=1, relheight=1)
 frame.pack()
 
+#home page image
 home = tk.PhotoImage(file="home.png", height=500)
 home1 = tk.Label(frame, image=home,bg="#FFFFFF")
 home1.pack()
 #home1.place(relx=0.2,rely=0, relwidth=0.6, relheight=0.6)
 
+translateButton = tk.Button(root, text="T R A N S L A T E", bg="#16a9ff", font=("oemfixed", 10, "bold"), bd=2, height=1, width=20, padx=0, pady=0)
+#translateButton = tk.Button(root, text="", bd=2, height=1, width=20, padx=0, pady=0)
+translateButton.pack()
 
 
 root.mainloop()
---
+++ test.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import PIL
+#import Pillow
+from PIL import ImageTk, Image
+
+class CanvasButton:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.number = tk.IntVar()
+        self.button = tk.Button(canvas, textvariable=self.number,
+                                command=self.buttonclicked)
+        self.id = canvas.create_window(50, 100, width=25, height=25,
+                                       window=self.button)
+    def buttonclicked(self):
+        self.number.set(self.number.get()+1)  # auto updates Button
+
+root = tk.Tk()
+#root.resizable(width=False, height=False)
+#root.wm_attributes("-topmost", 1)
+
+imgpath = 'home.png'
+img = Image.open(imgpath)
+photo = ImageTk.PhotoImage(img)
+
+canvas = tk.Canvas(root, bd=0, highlightthickness=0, height=500, width=500)
+canvas.pack()
+#create_image(x, y, anchor = NW, image = can.tab[nb]['image'])
+canvas.create_image(1000, 10000, image=photo)
+
+CanvasButton(canvas)  # create a clickable button on the canvas
+
+root.mainloop()

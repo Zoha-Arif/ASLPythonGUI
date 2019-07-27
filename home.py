ae84294d73d0b71dd1167fc2e73638f44c29b351
Zoha-Arif <Zoha-Arif@users.noreply.github.com>
2019-07-26 21:44:10 -0400

    r

--- home.py
+++ home.py
@@ -13,14 +13,6 @@ canvas.pack()
 frame = tk.Frame(root, bg="#b5d6b2")
 frame.place(relwidth=1, relheight=1)
 
-comb = tk.PhotoImage(file="comb.png", height=400)
-comb1 = tk.Label(frame, image=comb,bg="#b5d6b2")
-comb1.place(relx=0.2,rely=0, relwidth=0.6, relheight=0.6)
-
-text = tk.PhotoImage(file="text.png")
-text1 = tk.Label(frame, image=text,bg="#b5d6b2", height=100, width=500)
-text1.place(relx=0.3, rely=0.3, relwidth=0.4, relheight=0.4)
-
 
 
 root.mainloop()

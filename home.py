12c56c120a78153d0e9708fd024a7ea16d95dfa0
Zoha-Arif <Zoha-Arif@users.noreply.github.com>
2019-07-28 16:13:49 -0400

    added pb and tensorflow files

--- home.py
+++ home.py
@@ -1,22 +1,77 @@
 import tkinter as tk
+from PIL import ImageTk, Image
+import cv2
 import requests
 import random 
 
+#Arranging the HomePage
+FILENAME = 'home.png'
 root = tk.Tk()
-#widthxheight
-root.geometry("750x490")
-frame = tk.Frame(root, bg="#FFFFFF")
-frame.pack()
+canvas = tk.Canvas(root, width=750, height=490)
+canvas.pack()
+tk_img = ImageTk.PhotoImage(file = FILENAME)
+#canvas.create_image(125, 125, image=tk_img)
+#canvas.create_image(200, 400, image=tk_img, anchor='nw')
+canvas.create_image(0, 0, image=tk_img, anchor='nw')
 
-#home page image
-home = tk.PhotoImage(file="home.png", height=500)
-home1 = tk.Label(frame, image=home,bg="#FFFFFF")
-home1.pack()
-#home1.place(relx=0.2,rely=0, relwidth=0.6, relheight=0.6)
 
-translateButton = tk.Button(root, text="T R A N S L A T E", bg="#16a9ff", font=("oemfixed", 10, "bold"), bd=2, height=1, width=20, padx=0, pady=0)
-#translateButton = tk.Button(root, text="", bd=2, height=1, width=20, padx=0, pady=0)
-translateButton.pack()
+def translate():
+    frameNumber = tk.Frame(root, bg="#FFD966")
+    frameNumber.place(relwidth=1, relheight=1)
+    lmain = tk.Label(frameNumber)
+    lmain.grid(row=0, column=0)
+    cap = cv2.VideoCapture(0)
+    def show_frame():
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        img = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        lmain.imgtk = imgtk
+        lmain.configure(image=imgtk)
+        lmain.after(10, show_frame) 
+    #sliderFrame = tk.Frame(root, width=600, height=100)
+    #sliderFrame.pack(row = 600, column=0, padx=10, pady=2)
+    #sliderFrame.pack()
+    show_frame()  #Display 2
+    
 
+#Translate Button
+translate_button = tk.Button(root, highlightthickness=0, text = "    T R A N S L A T E", command = translate, anchor = 'w',
+                    width = 16, activebackground = "#16A9FF", bd=0, bg="#16A9FF",fg="#FFFFFF", font='Arial 9 bold')
+translate_button_window = canvas.create_window(33, 43, anchor='nw', window=translate_button)    
+
+#Sad Face/Help Button
+image = Image.open("helpb.png")
+image = image.resize((44, 44), Image.ANTIALIAS) ## The (250, 250) is (height, width)
+#pw.pic = ImageTk.PhotoImage(image)
+helpb = tk.Button(root, width=5, height=0, command = root.quit)
+#image1 = ImageTk.PhotoImage(file="helpb.png")
+image1 = ImageTk.PhotoImage(image)
+# Color Code = #FFD966
+helpb.config(image=image1, highlightthickness=0,  activebackground = "#FFD966", bd=0, bg="#FFD966", width=50, height=50)#These set the height and width of box of the picture.
+helpb.image = image1
+helpb_window = canvas.create_window(200, 30, anchor='nw', window=helpb) #These numbers move the picture. 
+
+#Heart/Information Button
+image = Image.open("heartb.png")
+image = image.resize((38, 38), Image.ANTIALIAS) ## The (250, 250) is (height, width)
+heartb = tk.Button(root, width=5, height=0, command = root.quit)
+#image1 = ImageTk.PhotoImage(file="helpb.png")
+image2 = ImageTk.PhotoImage(image)
+# Color Code = #FFD966
+heartb.config(image=image2, highlightthickness=0,  activebackground = "#FFD966", bd=0, bg="#FFD966", width=50, height=50) #These set the height and width of box of the picture.
+heartb.image = image2
+heartb_window = canvas.create_window(680, 30, anchor='nw', window=heartb) #These numbers move the picture. 
+
+
+#while True: 
+#        ret, frame = cap.read()
+#        cv2.imshow('frame', frame)
+#
+#        if cv2.waitKey(1) & 0xFF == ord('q'):
+#            break 
+#    cap.release()
+#    cv2.destroyAllWindows()
 
 root.mainloop()
